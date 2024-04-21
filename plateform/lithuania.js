8940132c65d1c5b32bbb7fd2432f656b5b5b5d96
Tasluf665 <tasluf15-12089@diu.edu.bd>
2024-04-22 01:49:06 +0600

    cc

--- plateform/lithuania.js
+++ plateform/lithuania.js
@@ -1,9 +1,7 @@
 // require('dotenv').config({ path: `../.env` });
 require('events').EventEmitter.defaultMaxListeners = 15;
 const fs = require('fs');
-const useProxy = require('puppeteer-page-proxy');
 const puppeteer = require("puppeteer");
-var userAgent = require('user-agents');
 const axios = require('axios');
 var FormData = require('form-data');
 const Captcha = require("@2captcha/captcha-solver")
@@ -38,10 +36,21 @@ const captchaSolver = async (page) => {
             .then(async (res) => {
                 console.log(res);
                 await page.$eval('.captcha-container .form-control', (input, value) => input.value = value, res.data);
-                await page.click('.col-12.text-center.mt-3.pb-3 button:nth-child(2)');
 
-                await page.waitForTimeout(15000);
-                appointment(page)
+                try {
+                    await page.waitForSelector('button.btn.primary.rounded-pill[data-submit-url="/en/actions/user/login/login"]')
+                    await page.$eval('button.btn.primary.rounded-pill[data-submit-url="/en/actions/user/login/login"]', element => element.click())
+
+                    await page.waitForSelector('button.btn.outline-primary.rounded-pill[data-url="/en/legalisation/wizard/1/-1"]')
+                    appointment(page)
+                } catch (error) {
+                    await page.$eval('a.reset-captcha-btn', element => element.click());
+                    await page.waitForTimeout(10000);
+                    captchaSolver(page)
+                }
+
+                // await page.click('button.btn.primary.rounded-pill[data-submit-url="/en/actions/user/login/login"]');
+
 
             })
 
@@ -54,37 +63,129 @@ const captchaSolver = async (page) => {
 }
 
 const appointment = async (page) => {
-    console.log("Redirect into legalisation page")
-    await page.goto('https://keliauk.urm.lt/en/legalisation');
-    await page.waitForTimeout(15000);
-
     console.log("Click into Register new visit")
-    await page.click('button.btn.outline-primary');
-    await page.waitForTimeout(15000);
+    try {
+        await page.waitForSelector('button.btn.outline-primary.rounded-pill[data-url="/en/legalisation/wizard/1/-1"]')
+        await page.click('button.btn.outline-primary.rounded-pill[data-url="/en/legalisation/wizard/1/-1"]');
+    } catch (error) {
+        await page.waitForSelector('button.btn.outline-primary.rounded-pill[data-url="/en/legalisation/wizard/1/-1"]')
+        await page.click('button.btn.outline-primary.rounded-pill[data-url="/en/legalisation/wizard/1/-1"]');
+    }
+
 
     console.log("Click into next btn")
-    await page.click('#be1c2070729538c4196e7274ed27309d7');
-    await page.waitForTimeout(15000);
+    try {
+        await page.waitForSelector('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]')
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    } catch (error) {
+        await page.waitForSelector('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]')
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    }
+
 
     console.log("Select Participate")
-    // await page.click('input.xradio-card-control[value="WithFiz"]');
-    const [element] = await page.$x('//*[@id="content"]/div/form/div[1]/div[1]/div[2]/label');
-    await element.click();
-    await page.waitForTimeout(10000);
+    try {
+        await page.waitForSelector('.xradio-card-checkbox')
+        const secondElement = await page.$$('.xradio-card-checkbox');
+        await secondElement[1].click();
+    } catch (error) {
+        await page.waitForSelector('.xradio-card-checkbox')
+        const secondElement = await page.$$('.xradio-card-checkbox');
+        await secondElement[1].click();
+    }
+
+
 
     console.log("Click into next btn2")
-    await page.click('button.btn.btn-primary.rounded-pill');
-    await page.waitForTimeout(10000);
+    try {
+        await page.waitForSelector('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]')
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    } catch (error) {
+        await page.waitForSelector('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]')
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    }
 
-    const [element2] = await page.$x('//*[@id="content"]/div/form/div[1]/div/div/div/div[1]/label/div[1]/div/div');
-    await element2.click();
-    await page.waitForTimeout(10000);
+    try {
+        await page.waitForSelector('input[value="206"]')
+        await page.click('input[value="206"]');
+    } catch (error) {
+        await page.waitForSelector('input[value="206"]')
+        await page.click('input[value="206"]');
+    }
 
     console.log("Click into next btn3")
-    await page.click('button.btn.btn-primary.rounded-pill');
-    await page.waitForTimeout(10000);
+    try {
+        await page.waitForSelector('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]')
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    } catch (error) {
+        await page.waitForSelector('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]')
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    }
+
+    console.log("Fill up the form")
+    await page.waitForSelector('input.form-control[is="number-phone"]')
+    await page.$eval('input.form-control[is="number-phone"]', (input, value) => input.value = value, process.env.FORM_PHONE);
+    await page.$eval('input.form-control[name="User[1][Name]"]', (input, value) => input.value = value, process.env.FORM_FIRST_NAME);
+    await page.$eval('input.form-control[name="User[1][Surname]"]', (input, value) => input.value = value, process.env.FORM_LAST_NAME);
+    await page.$eval('input.form-control[name="User[1][Email]"]', (input, value) => input.value = value, process.env.FORM_EMAIL);
+    await page.$eval('input.form-control[name="User[1][Phone]"]', (input, value) => input.value = value, process.env.FORM_USER_PHONE);
+    await page.$eval('textarea.form-control[name="User[1][Purpose]"]', (input, value) => input.value = value, process.env.FORM_Representation);
 
 
+    try {
+        const CountrySelect1 = await page.$$('.xselect-display.form-control');
+        await CountrySelect1[0].click();
+
+        await page.waitForSelector('.xselect-list-item[data-value="25"]')
+        await page.click('.xselect-list-item[data-value="25"]')
+    } catch (error) {
+        const CountrySelect1 = await page.$$('.xselect-display.form-control');
+        await CountrySelect1[0].click();
+
+        await page.waitForSelector('.xselect-list-item[data-value="25"]')
+        await page.click('.xselect-list-item[data-value="25"]')
+    }
+
+    try {
+        const CountrySelect2 = await page.$$('.xselect-display.form-control');
+        await CountrySelect2[1].click();
+
+        const Country = await page.$$('.xselect-list-item[data-value="182"]');
+        await Country[1].click();
+
+
+    } catch (error) {
+        const CountrySelect2 = await page.$$('.xselect-display.form-control');
+        await CountrySelect2[1].click();
+
+        const Country = await page.$$('.xselect-list-item[data-value="182"]');
+        await Country[1].click();
+    }
+
+    try {
+        console.log("Click into next btn4")
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    } catch (error) {
+        console.log("Click into next btn4")
+        await page.click('button.btn.btn-primary.rounded-pill[data-submit-url="/en/actions/legalisation/insert"]');
+    }
+
+    try {
+        await page.waitForSelector('input.form-control[is="control-picker-date-remote"]')
+        console.log("Click into next btn4")
+        await page.click('input.form-control[is="control-picker-date-remote"]');
+    } catch (error) {
+        console.log("Click into next btn4")
+        await page.click('input.form-control[is="control-picker-date-remote"]');
+    }
+
+    try {
+        await page.click('#date');
+        await page.type('#date', '2024-04-10');
+        await page.click('body');
+    } catch (error) {
+
+    }
 }
 
 async function login(url) {
@@ -112,9 +213,11 @@ async function login(url) {
             waitUntil: 'load', timeout: 0
         });
 
-        await page.waitForTimeout(25000);
+        await page.waitForSelector('button.btn.outline-primary.rounded-pill[is="button-url"]');
+        await page.click('button.btn.outline-primary.rounded-pill[is="button-url"]');
 
-        await page.$eval('.url-input-wrap .form-control', (input, value) => input.value = value, process.env.VFS_EMAIL);
+        await page.waitForSelector('input.form-control[is="text-mail"]')
+        await page.$eval('input.form-control[is="text-mail"]', (input, value) => input.value = value, process.env.VFS_EMAIL);
         await page.$eval('input[type="password"]', (input, value) => input.value = value, process.env.VFS_PASSWORD);
 
 
@@ -124,13 +227,14 @@ async function login(url) {
 
     } catch (error) {
         console.log("Loging Error: ", error.message);
+        login(url)
     }
 }
 
 
 const lithuania = async () => {
     console.log('calling API ----- ')
-    const url = "https://keliauk.urm.lt/en/user/login"
+    const url = "https://keliauk.urm.lt/en/legalisation"
     await login(url)
 }
 
